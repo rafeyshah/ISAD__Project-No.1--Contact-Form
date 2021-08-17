0f274f5757305701218b094a346fed3722d455dd
Your Name <csyedabdulrafeyali@gmail.com>
2021-08-17 19:18:02 +0500

    remove__bug1

--- confirm_password.js
+++ confirm_password.js
@@ -1,5 +1,5 @@
 var check = function () {
-    if (document.getElementById('password').value == document.getElementById('confirm_password').value) {
+    if (document.getElementById('pass').value == document.getElementById('c_pass').value) {
         document.getElementById('message').style.color = 'green';
         document.getElementById('message').innerHTML = 'matching';
     } 
@@ -7,4 +7,29 @@ var check = function () {
         document.getElementById('message').style.color = 'red';
         document.getElementById('message').innerHTML = 'not matching';
     }
+}
+
+var myFunction1 = function (){
+    var x = document.getElementById("f_name");
+    x.classList.add("focus");
+}
+
+var myFunction2 = function (){
+    var x = document.getElementById("l_name");
+    x.classList.add("focus");
+}
+
+var myFunction3 = function (){
+    var x = document.getElementById("email");
+    x.classList.add("focus");
+}
+
+var myFunction4 = function (){
+    var x = document.getElementById("password");
+    x.classList.add("focus");
+}
+
+var myFunction5 = function (){
+    var x = document.getElementById("confirm_password");
+    x.classList.add("focus");
 }
